669b38018dfda98b516bbdb79b25525ec9ec6104
diyorbek-7 <d.shokirov@newuu.uz>
2025-02-12 17:47:51 +0500

    homework

--- lesson-3/homework/dictionary_task.py
+++ lesson-3/homework/dictionary_task.py
@@ -1,84 +1,146 @@
-d1 = {'v1':'k1','v2':'k2','v3':'k3'}
+d1 = {'v2':'k1','v1':'k2','v3':'k3'}
 d2 = {'a1':'b1','a2':'b2','a3':'b3'}
+d3 = {1:1,2:2,3:3}
 k ='v1'
 #1
-# k = "v1"
-# if k in d1:
-#     print(d1[k])
-# else:
-#     "not found"
+k = "v1"
+if k in d1:
+    print(d1[k])
+else:
+    "not found"
 
 #2
-# print(bool(k in d1))
 
 #3
-# print(len(d1))
+print(len(d1))
+l = list(d1.keys())
+print(l)
 
 #4
-# l = list(d1.keys())
-# print(l)
+print(bool(k in d1))
+
+
 
 #5
-# l = list(d1.values())
-# print(l)
+l = list(d1.values())
+print(l)
 
 #6
-# d = dict(d1 | d2)
-# print(d)
+d = dict(d1 | d2)
+print(d)
 
 # #7
-# if k in d1:
-#     d1.pop(k)
-#     print('removed')
-# else:
-#     print('not found')
-# print(d1)
+if k in d1:
+    d1.pop(k)
+    print('removed')
+else:
+    print('not found')
+print(d1)
 
 #8
-# d = dict()
-# print(type(d))
+d = dict()
+print(type(d))
 
 #9
-# if d1:
-#     print('not empty')
-# else:
-#     print('empty')
+if d1:
+    print('not empty')
+else:
+    print('empty')
 
 #10
-# if k in d1:
-#     p = (k,d1[k])
-#     print(p)
-# else:
-#     print('no key')
+if k in d1:
+    p = (k,d1[k])
+    print(p)
+else:
+    print('no key')
 
 # #11
-# d1[k] = 'c1'
-# print(d1)
+d1[k] = 'c1'
+print(d1)
 
 #12
-# c = list(d1.values()).count("k1")
-# print(c)
+c = list(d1.values()).count("k1")
+print(c)
 
 #13
-# k = {}
-# for  n,m in d1.items():
-#     k[m]=n
-# print(k)
+k = {}
+for  n,m in d1.items():
+    k[m]=n
+print(k)
 
 #14
-# a = 'k2'
-# l = []
-# for n,m in d1.items():
-#     if m==a:
-#         l.append(n)
-# print(l)
+a = 'k2'
+l = []
+for n,m in d1.items():
+    if m==a:
+        l.append(n)
+print(l)
 
 #15
-# l=[1,2,3,4,5]
-# l2=[1,2,3,4,5]
-# d = dict()
-# for i in range(len(l)):
-#     d[l[i]]=l2[i]
-# print(d)
+l=[1,2,3,4,5]
+l2=[1,2,3,4,5]
+d = dict()
+for i in range(len(l)):
+    d[l[i]]=l2[i]
+print(d)
 
 #16
+for i in d1.keys():
+    if type(i)==dict:
+        print(True)
+        break
+else:
+        print(False)
+
+#17
+d = {1: 'v', 2: 'v', 3: {'v1':'k1','v2':'k2','v3':'k3'}}
+n = d[3]['v1']
+print(n)
+
+#18
+k = 'v1'
+
+if k in d1 :
+        v = d1[k]
+else:
+        v = 'default'
+print(v)
+
+#19
+c = len(set(d1.values()))
+print(c)
+
+#20
+d = dict(sorted(d1.items()))
+print(d)
+
+#21
+
+
+#22
+filtr = {}
+for k,v in d3.items():
+    if v > 2:
+        filtr[k] = v
+print(filtr)
+
+#23
+for i in d1:
+    if i in d2:
+        print(True)
+    else:
+        print(False)
+
+#24
+t = ((1,'a'),(2,'b'),(3,'c'))
+d = {}
+for i,j in t:
+    d[i] = j
+print(d)
+
+#25
+for i,j in d1:
+    pair = (i,j)
+    break
+print(pair)
+
--- lesson-3/homework/tuple_tasks.py
+++ lesson-3/homework/tuple_tasks.py
@@ -2,122 +2,122 @@ t1=(1,2,3,4,4,5,6,8,4,5,6)
 t2 = (1,2,4,5,4,5,6,5,8,4,5,3,1,1)
 
 #1
-# e = 4
-# print(t1.count(e))
+e = 4
+print(t1.count(e))
 
 # #2
-# m = t1[0]
-# for i in t1:
-#     if i > m:
-#         m = i
-# print(m)
+m = t1[0]
+for i in t1:
+    if i > m:
+        m = i
+print(m)
 #3
-# m = t1[0]
-# for i in t1:
-#     if i < m:
-#         m = i
-# print(m)
+m = t1[0]
+for i in t1:
+    if i < m:
+        m = i
+print(m)
 
 #4
-# e = 1
-# print(e in t1)
+e = 1
+print(e in t1)
 
 #5
-# print(t1[0] if t1 else "empty")
+print(t1[0] if t1 else "empty")
 
 #6
-# print(t1[-1] if t1 else "empty")
+print(t1[-1] if t1 else "empty")
 
 #7
-# print(len(t1))
+print(len(t1))
 
 #8
-# t = tuple(t1[:3])
-# print(t)
+t = tuple(t1[:3])
+print(t)
 
 #9
-# t = t1+t2
-# print(t)
+t = t1+t2
+print(t)
 
 #10
-# print(t1 if t1 else "Empty")
+print(t1 if t1 else "Empty")
 
 #11
-# e = 4
-# ind = []
-# for i in range (len(t1)):
-#     if t1[i] == e:
-#         ind.append(i)
-# print(ind)
+e = 4
+ind = []
+for i in range (len(t1)):
+    if t1[i] == e:
+        ind.append(i)
+print(ind)
 
 #12
-# increasing = sorted(set(t1))
-# print(increasing[-2] if len(increasing) > 1 else "no 2nd largest")
+increasing = sorted(set(t1))
+print(increasing[-2] if len(increasing) > 1 else "no 2nd largest")
 
 #13
-# dec = sorted(set(t1))
-# print(dec[2] if len(dec) >=2 else "no 2nd smallest")
+dec = sorted(set(t1))
+print(dec[2] if len(dec) >=2 else "no 2nd smallest")
 
 #14
-# t = (1,)
-# print(len(t))
-# print(type(t))
+t = (1,)
+print(len(t))
+print(type(t))
 
 #15
-# list1 = [1,2,3,4,5,6,8,4,5,3,1,1]
-# t = tuple(list1)
-# print(t)
+list1 = [1,2,3,4,5,6,8,4,5,3,1,1]
+t = tuple(list1)
+print(t)
 
 #16
-# m = sorted(set(t1))
-# print(m == t1)
+m = sorted(set(t1))
+print(m == t1)
 
 #17
-# t = ((1,2,3),(4,5,6),(7,8,9))
-# t12 = t [1]
-# m = t12[0]
-# for i in t12:
-#     if i > m:
-#         m = i
-# print(m)
+t = ((1,2,3),(4,5,6),(7,8,9))
+t12 = t [1]
+m = t12[0]
+for i in t12:
+    if i > m:
+        m = i
+print(m)
 
 #18
-# t = ((1,2,3),(4,5,6),(7,8,9))
-# t12 = t[0]
-# g = t12[0]
-# for i in t12:
-#     if i < g:
-#         g = i
-# print(g)
+t = ((1,2,3),(4,5,6),(7,8,9))
+t12 = t[0]
+g = t12[0]
+for i in t12:
+    if i < g:
+        g = i
+print(g)
 
 #19
-# t = 5
-# for i in range(len(t1)):
-#     if t1[i] == t:
-#         print(t1[:i]+t2[:i])
-#         break
+t = 5
+for i in range(len(t1)):
+    if t1[i] == t:
+        print(t1[:i]+t2[:i])
+        break
 
 #20
 #-
 
 
 #21
-# n = 2
-# l = []
-# for i in t1:
-#     for j in range(n):
-#         l.append(i)
-# t = tuple(l)
-# print(t)
+n = 2
+l = []
+for i in t1:
+    for j in range(n):
+        l.append(i)
+t = tuple(l)
+print(t)
 
 #22
-# n = 1
-# m = 11
-# l = []
-# for i in range(n,m+1):
-#     l.append(i)
-# t = tuple(l)
-# print(t)
+n = 1
+m = 11
+l = []
+for i in range(n,m+1):
+    l.append(i)
+t = tuple(l)
+print(t)
 
 #23
 l = t1[::-1]
@@ -125,15 +125,15 @@ print(l)
 
 #24
 
-# t = (1,2,3,4,3,2,1)
-# print(t[::]==t[::-1])
+t = (1,2,3,4,3,2,1)
+print(t[::]==t[::-1])
 
 #25
-# l = []
-# for i in  t1:
-#     if i not in l:
-#         l.append(i)
-# t = tuple(l)
-# print(t)
+l = []
+for i in  t1:
+    if i not in l:
+        l.append(i)
+t = tuple(l)
+print(t)
 
 
