12d12182c7570348ef89d0ced4a3cd79e035b4d5
diyorbek-7 <d.shokirov@newuu.uz>
2025-02-18 20:29:18 +0500

    homework

---
+++ lesson-6/homework/lesson-6.py
@@ -0,0 +1,23 @@
+#zero check decorator
+def check(func):
+    def wrapper(a,b):
+        if b == 0 :
+            return 'Denominator can\'t be zero'
+        else:
+            return func(a,b)
+    return wrapper
+@check
+def div(a,b):
+    return a/b
+print(div(2,3))
+
+#Emoloyee Records Manager
+with open('employees.txt',mode='a') as f:
+    id = int(input('Enter employee ID: '))
+    name = input('Enter employee name: ')
+    position = input('Enter employee position: ')
+    salary = int(input('Enter employee salary: '))
+    f.write(f'{id},{name},{position},{salary}\n')
+f.close()
+
+#
