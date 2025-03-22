3d178d040f605806492b2f20fc44eee4900d28e5
diyorbek-7 <d.shokirov@newuu.uz>
2025-03-22 21:01:43 +0500

    homework

--- demo/demo.py
+++ demo/demo.py
@@ -1,8 +1,3 @@
-import requests
-apr = 'ad513c94c2474c017ee4298342b5702c'
-url = 'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
-res = requests.get(url)
-if res.status_code==200:
-    data = res.json()
-
-
+import numpy as np
+def powerr(n,p):
+    return n**p
